b0019cd45657408ee9423eaf5199bcd8721a7ed5
rmzimmerman <rmzimmerman06@gmail.com>
2023-03-16 19:04:26 -0400

    routes established

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose')
 const methodOverride = require('method-override')
 const seed = require('./models/marksmen.js')
-const MarksmanSchema = require('./models/markschema.js')
+const MarksmanSchema = require('./models/markschema.js');
 const app = express();
 
 app.use(express.static('public'))
@@ -27,6 +27,45 @@ app.get('/marksmen', (req, res) => {
         })
     })
 })
+
+app.get('/marksmen/new', (req, res) => {
+    res.render('new.ejs')
+})
+
+app.get('/:id', (req, res) => {
+    MarksmanSchema.findById(req.params.id).then((theMarksman) => {
+        res.render('show.ejs', {
+            Marksman: theMarksman
+        })
+    })
+})
+
+app.get('/:id/edit', (req, res) => {
+    MarksmanSchema.findById(req.params.id).then((theMarksman) => {
+        res.render('edit.ejs', {
+            Marksman: theMarksman
+        })
+    })
+})  
+
+app.post('/marksmen', (req, res) => {
+    MarksmanSchema.create(req.body).then((createdMarksman) => {
+        res.redirect('/marksmen')
+    })
+})
+
+app.delete('/:id', (req, res) => {
+    MarksmanSchema.findByIdAndRemove(req.params.id).then(() => {
+        res.redirect('/marksmen')
+    })
+})
+
+app.put('/:id', (req, res) => {
+    MarksmanSchema.findByIdAndUpdate(req.params.id, req.body, {new:true}).then(() => {
+        res.redirect('/marksmen')
+    })
+})
+
 
 mongoose.connect('mongodb://127.0.0.1:27017/marksmen').then (() => {
   console.log('The connection with mongod is established')
